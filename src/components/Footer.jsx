f19509187016b33f7af4aaa98d4fc3178930cee9
cdevl3749 <cdevl3749@gmail.com>
2025-12-07 02:00:06 +0100

    modif footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -137,6 +137,33 @@ export default function Footer() {
           </p>
         </div>
 
+        {/* Réseaux sociaux */}
+<div className="bp-footer-socials">
+  <a
+    href="https://www.linkedin.com/in/christophe-devleeshouwer-882377399/"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="bp-social-link"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="28"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 
+      5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 
+      0-1.75-.79-1.75-1.764s.784-1.764 
+      1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 
+      1.764zm13.5 11.268h-3v-5.604c0-3.368-4-3.113-4 
+      0v5.604h-3v-10h3v1.765c1.396-2.586 7-2.777 
+      7 2.476v5.759z"/>
+    </svg>
+  </a>
+</div>
+
+
         {/* Copyright */}
         <div className="bp-footer-bottom">
           <p>© {year} BlockPulse.be — Tous droits réservés</p>

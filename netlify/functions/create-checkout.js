f7f51ff64c3e4af3fa252547b051ce45897701af
cdevl3749 <cdevl3749@gmail.com>
2026-04-07 17:08:16 +0200

    fix stripe netlify

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
 
 const EU_COUNTRIES = [
   "AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR",
